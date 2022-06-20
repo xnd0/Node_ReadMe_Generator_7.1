12d2b4795ff2892a81752524b1b16458ad09a42b
xndcmd <xndcmd@gmail.com>
2022-06-20 08:57:08 -0700

    added license badges for MIT + Mozilla at top

--- index.js
+++ index.js
@@ -73,6 +73,10 @@ function writeToFile(fileName, data) {
 
 ## Title: ${data.title}
 <br>
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
     
 ## Description:
 ${data.description}
