a19382e21eaa94de04bebd91dff232f98389fdfe
xndcmd <xndcmd@gmail.com>
2022-06-19 17:10:16 -0700

    added filler questions for inquirer prompt. Successfully launches

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your user name?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Where are you located?',
+        name: 'location',
+    },
+    {
+        type: 'input',
+        message: 'What coding language do you use?',
+        name: 'language',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions);
+}
 
 // Function call to initialize app
 init();
